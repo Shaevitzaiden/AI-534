563de0656f5124037169f4169ad1fd399db7d997
Jostan86 <jostanbrown86@gmail.com>
2022-10-21 11:22:24 -0700

    Updated plotting function, added function to plot accuracy

--- HW02/HW02.py
+++ HW02/HW02.py
@@ -13,6 +13,7 @@ class LRM():
         self.weights = None
         self.best_weights = None
         self.mse_ot = None
+        self.num_iterations = None
 
     def train(self, x, y, converge_thresh, max_iterations, lr, reg_param, method="l2") -> np.array:
         """
@@ -26,7 +27,7 @@ class LRM():
         num_features = x.shape[1]
         
         # Initialize weights as 1 (could change to random in the future)
-        weights = np.ones((num_features,)) # create vertical vector of weights
+        weights = np.zeros((num_features,)) # create vertical vector of weights
 
         mse_best = np.Inf
         mse_ot = []
@@ -36,6 +37,7 @@ class LRM():
         #[[x00 x10 x20 x30 ..]   === [y_hat1, y_hat2]
         # [x01 ]]
         delta_error = np.Inf
+        mse_prev = np.Inf
         iterations = 0
         while (delta_error > converge_thresh) and (iterations < max_iterations):
             # Compute error (e = y_hat - y)
@@ -53,13 +55,17 @@ class LRM():
                 mask = reg_vals > 0
                 weights[1:] = np.sign(weights[1:]) * (reg_vals * mask)
 
+            mse = self.get_mse(weights, x, y)
+            mse_ot.append(mse)
 
-            mse_ot.append(self.get_mse(weights, x, y))
+            delta_error = mse_prev - mse
+            mse_prev = mse
             iterations += 1
             
 
         self.weights = weights
         self.mse_ot = mse_ot
+        self.num_iterations = iterations
         return mse_ot
 
     def eval(self, x, y) -> float:
@@ -69,6 +75,7 @@ class LRM():
         return np.sum(prediction == y)
     
     def predict(self, x) -> np.array:
+
         # num_samples = x.shape[0]
         # x = np.hstack((x,np.ones((num_samples,1))))
         return self.sigmoid(np.dot(x, self.weights)) # ----------------------------- double check if sigmoid should be here
@@ -111,17 +118,32 @@ def load_data(path, normalize=True, remove_col=None, test=False):
 
     return x, y
 
-def plot_mse(mse_arrays, lrs):
+def plot_mse(mse_arrays, lrs, reg_params):
     fig, ax = plt.subplots()
 
     # plot lines and add legend labels to them
     lines = []
-    for mse, lr in zip(mse_arrays, lrs):
-        lines.append(ax.plot(mse, label=str(lr)))
-    ax.legend(title="lrs", fontsize="small", fancybox=True, loc='upper right')
+    for mse, lr, reg_param in zip(mse_arrays, lrs, reg_params):
+        lines.append(ax.plot(mse, label=str(lr)+';  ' + str(reg_param)))
+    ax.legend(title="LR; RP", fontsize="small", fancybox=True, loc='upper right')
     ax.set_ylabel("MSE")
     ax.set_xlabel("Iteration")
     ax.set_title("MSE vs Training Iterations")
+    plt.show()
+    return fig, ax
+
+def plot_accuracy(train, test, reg_params):
+    fig, ax = plt.subplots()
+
+    # plot lines and add legend labels to them
+    lines = []
+    ax.plot(reg_params, train, label='train')
+    ax.plot(reg_params, test, label='test')
+    ax.legend(title="Dataset", fontsize="small", fancybox=True, loc='upper right')
+    ax.set_ylabel("Accuracy")
+    ax.set_xlabel("Regularization Parameter")
+    ax.set_title("Accuracy vs. Regularization Parameter")
+    ax.set_xscale('log')
     plt.show()
     return fig, ax
 
@@ -140,21 +162,31 @@ def write_results_to_csv(file_name, ids, predictions):
 
 if __name__ == "__main__":
     # month=0, day=1, zipcode=16, lat=17, long=18, sq_living15=20
+
     rm_cols = None # or None
-    X, Y= load_data("HW02\IA2-train.csv")    
-    
-    model = LRM()
-    loss_ot = model.train(X,Y,10e-2, 300, 10e-1, 0.01, method='l2')
-    plot_mse([loss_ot], [10e-4])
-    
-    print(model.eval(X,Y)/6000)
-    # X_dev, Y_dev, ids_dev = load_data("HW01\IA1_dev.csv",remove_col=rm_cols)
-    # lrs = [10**-1, 10**-2, 10**-3, 10**-4]
-    
-    # house_models = [LRM() for i in range(len(lrs))] # create a model for each lr
-    
+    X, Y = load_data("IA2-train.csv")
+    X_test, Y_test = load_data("IA2-dev.csv")
+    u =10 ** -2
+    reg_params = 10 ** np.arange(-4, 3, dtype=float)
+    lrs = [10e-1, 10e-1, 10e-1, 10e-2, 10e-3, 10e-3, 10e-4]
+    converge_thresh = 10e-8
+    max_iterations = 3000
+    method = 'l2'
+    models = [LRM() for i in range(len(reg_params))]
+    loss_ot = [models[i].train(X, Y, converge_thresh, max_iterations, lrs[i], reg_params[i], method) for i in range(len(models))]
 
+    # plot_mse([loss_ot], [10e-4])
+    train_results = []
+    test_results = []
+    for i in range(len(reg_params)):
+        train_results.append(models[i].eval(X,Y)/6000)
+        test_results.append(models[i].eval(X_test, Y_test) / 10000)
+        print("Train: " + str(round(train_results[i], 4)) + " Test: " + str(round(test_results[i], 4)) + " iters: " + str(models[i].num_iterations))
 
+    mses = [np.array(models[i].mse_ot) for i in range(len(models))]
+
+    # plot_mse(mses, lrs, reg_params)
+    plot_accuracy(train_results, test_results, reg_params)
 
 
     

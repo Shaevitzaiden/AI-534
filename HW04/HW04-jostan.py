73b0199050627224cb0e2c7d010916efa6de4870
Jostan86 <jostanbrown86@gmail.com>
2022-12-01 18:47:38 -0800

    Add some comments

--- HW04/HW04-jostan.py
+++ HW04/HW04-jostan.py
@@ -47,17 +47,27 @@ if __name__ == "__main__":
     ge = GloVe_Embedder("GloVe_Embedder_data.txt")
     top_words = get_top_words(ge, seed_words, 30)
     # print_top_words(top_words, formatted=True)
+
+    # Get full list of just words
     words = []
     for word_list in top_words:
         for word in word_list:
             words.append(word[0])
+
+    # Get embedings for list of words
     embedings = ge.embed_list(words)
+
+    # Do PCA and plot it
     pca = PCA(n_components=2)
     principalComponents = pca.fit_transform(embedings)
     # plot_points(principalComponents[:, 0], principalComponents[:, 1])
+
+    # Do tsne and plot it
     tsne = TSNE(n_components=2, perplexity=25)
     z = tsne.fit_transform(embedings)
     # plot_points(z[:, 0], z[:, 1], words)
+
+    # Do kmeans
     inertias = []
     ns = [n for n in range(2,21)]
     fig, ax = plt.subplots()
@@ -72,6 +82,7 @@ if __name__ == "__main__":
         label_pred = km.labels_
         ari.append(sklearn.metrics.adjusted_rand_score(label_ground_truth, label_pred))
         nmi.append(sklearn.metrics.normalized_mutual_info_score(label_ground_truth, label_pred))
+        # Calculate purity
         max_class_in_cluster_sum = 0
         for i in range(n):
             start = 0
@@ -82,9 +93,8 @@ if __name__ == "__main__":
 
             max_class_in_cluster_sum += (max(num_class_in_cluster))
         purity.append(max_class_in_cluster_sum / 150)
-    print(purity)
 
-
+    # Plot inertiias
     ax.plot(ns, inertias)
     ax.set_xticks(ns)
     ax.set_ylim(0, 3000)

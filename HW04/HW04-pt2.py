96ff135f4653b804eedf299460769238c2f7d113
Jostan86 <jostanbrown86@gmail.com>
2022-12-02 18:39:05 -0800

    Fix HW04-pt2

--- HW04/HW04-pt2.py
+++ HW04/HW04-pt2.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 import re
-import sklearn as sk
-import seaborn as sns
+# import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
 from GloVe_Embedder import GloVe_Embedder
 
+ge = GloVe_Embedder("GloVe_Embedder_data.txt")
 
+meaningful_word_list = []
+for word in ge.embedding_dict:
+    embedding = ge.embed_str(word, indicate_unk=True, warn_unk=False)
+    if not embedding[1]:
+        meaningful_word_list.append(word)
+meaningful_word_list.extend([str(i) for i in range(1000)])
 
-# meaningful_word_list.extend([str(i) for i in range(1000)])
-
-def preprocess(data_train, data_dev, method="GloVe 1"):
+def preprocess(data_train, data_dev, method="GloVe 3"):
 
     if method == "TF-IDF":
         vectorizer = TfidfVectorizer(use_idf=True)
@@ -82,14 +85,6 @@ def preprocess(data_train, data_dev, method="GloVe 1"):
     elif method == "GloVe 3":
         ge = GloVe_Embedder("GloVe_Embedder_data.txt")
 
-        meaningful_word_list = []
-        for word in ge.embedding_dict:
-            embedding = ge.embed_str(word, indicate_unk=True, warn_unk=False)
-            if not embedding[1]:
-                meaningful_word_list.append(word)
-        meaningful_word_list.extend([str(i) for i in range(1000)])
-
-
         tweet_list_train = []
         tweet_list_dev = []
         for tweet in data_train:
@@ -105,7 +100,7 @@ def preprocess(data_train, data_dev, method="GloVe 1"):
                 if not embedding[1]:
                     embeddings = np.concatenate((embeddings, np.reshape(embedding[0], (1, -1))), axis=0)
                 else:
-                    new_words = split_words(word, meaningful_word_list, ge)
+                    new_words = split_words(word, ge)
                     for new_word in new_words:
                         embedding = ge.embed_str(new_word, indicate_unk=True)
                         if not embedding[1]:
@@ -121,7 +116,7 @@ def preprocess(data_train, data_dev, method="GloVe 1"):
                 if not embedding[1]:
                     embeddings = np.concatenate((embeddings, np.reshape(embedding[0], (1, -1))), axis=0)
                 else:
-                    new_words = split_words(word, meaningful_word_list, ge)
+                    new_words = split_words(word, ge)
                     for new_word in new_words:
                         embedding = ge.embed_str(new_word, indicate_unk=True)
                         if not embedding[1]:
@@ -131,7 +126,7 @@ def preprocess(data_train, data_dev, method="GloVe 1"):
 
     return X_train, X_dev
 
-def parse(data, meaningful_word_list, result=None):
+def parse(data, result=None):
     if result is None:
         result = []
     if data in meaningful_word_list:
@@ -143,9 +138,10 @@ def parse(data, meaningful_word_list, result=None):
             if last in meaningful_word_list:
                 yield from parse(first, result + [last])
 
-def split_words(word, meaningful_word_list, ge):
+def split_words(word, ge):
     word_options = []
-    for words in parse(word, meaningful_word_list):
+    # for words in parse(word, meaningful_word_list):
+    for words in parse(word):
         word_options.append(words)
 
     if len(word_options) > 0:
@@ -184,8 +180,7 @@ def linearSVM_compare(X_train, y_train, X_dev, y_dev, i_s, sv_plot=False):
     accuracy_train = []
     num_sv = []
     for i, c in enumerate(cs):
-        if c==1:
-            print('hi')
+
         linSVM = SVC(C=c, kernel='linear')
         linSVM.fit(X_train, y_train)
         predictions_test = linSVM.predict(X_dev)
@@ -194,6 +189,7 @@ def linearSVM_compare(X_train, y_train, X_dev, y_dev, i_s, sv_plot=False):
         accuracy_train.append(np.sum(predictions_train == y_train) / np.size(y_train))
         num_sv.append(linSVM.support_.size)
         print("i = {0}, c = {1}, accuracy = {2}%".format(i_s[i], np.round(c, 2), np.round(100 * accuracy_test[i], 3)))
+        print(accuracy_train[i])
 
     best_idx = np.argmax(accuracy_test)
     print(num_sv)
@@ -201,21 +197,21 @@ def linearSVM_compare(X_train, y_train, X_dev, y_dev, i_s, sv_plot=False):
     print("i = {0}, c = {1}, accuracy = {2}%".format(i_s[best_idx], np.round(cs[best_idx], 2),
                                                      np.round(100 * accuracy_test[best_idx], 3)))
 
-    plt.plot(np.log10(cs), accuracy_test, label='Validation')
-    plt.plot(np.log10(cs), accuracy_train, label='Train')
-    plt.xlabel("$log_{10}$(c)")
-    plt.ylabel("Accuracy")
-    plt.title("Linear SVM Accuracy vs c")
-    plt.legend()
-    plt.show()
-
-    if sv_plot:
-        plt.clf()
-        plt.plot(np.log10(cs), num_sv)
-        plt.xlabel("$log_{10}$(c)")
-        plt.ylabel("Number of Support Vectors")
-        plt.title("Number of Support Vectors for Varying C-Values")
-        plt.show()
+    # plt.plot(np.log10(cs), accuracy_test, label='Validation')
+    # plt.plot(np.log10(cs), accuracy_train, label='Train')
+    # plt.xlabel("$log_{10}$(c)")
+    # plt.ylabel("Accuracy")
+    # plt.title("Linear SVM Accuracy vs c")
+    # plt.legend()
+    # plt.show()
+    #
+    # if sv_plot:
+    #     plt.clf()
+    #     plt.plot(np.log10(cs), num_sv)
+    #     plt.xlabel("$log_{10}$(c)")
+    #     plt.ylabel("Number of Support Vectors")
+    #     plt.title("Number of Support Vectors for Varying C-Values")
+    #     plt.show()
 
     return accuracy_test
 
@@ -243,6 +239,7 @@ def rbfSVM_compare(X_train, y_train, X_dev, y_dev, ics, igs, heatmap=True, sv_pl
             accuracy_dev[row, col] = (np.sum(predictions_dev == y_dev) / np.size(y_dev))
             print("c = {0}, g = {1}, accuracy = {2}%".format(np.round(c, 2), np.round(g, 2),
                                                              np.round(100 * accuracy_dev[row, col], 3)))
+            print(accuracy_train[row, col])
             if np.isclose(c, 10):
                 sv_c_constant.append(rbfSVM.support_.size)
             if np.isclose(g, 0.1):
@@ -253,41 +250,41 @@ def rbfSVM_compare(X_train, y_train, X_dev, y_dev, ics, igs, heatmap=True, sv_pl
     print("c = {0}, g = {1}, accuracy = {2}%".format(np.round(cs[bc], 2), np.round(gs[br], 2),
                                                      np.round(100 * accuracy_dev[br, bc], 3)))
 
-    if heatmap:
-        f, (ax1, ax2, cax) = plt.subplots(1, 3, gridspec_kw={'width_ratios': [1, 1, 0.1], 'height_ratios': [1]})
-        min_val = np.min(np.hstack((accuracy_train, accuracy_train)))
+    # if heatmap:
+    #     f, (ax1, ax2, cax) = plt.subplots(1, 3, gridspec_kw={'width_ratios': [1, 1, 0.1], 'height_ratios': [1]})
+    #     min_val = np.min(np.hstack((accuracy_train, accuracy_train)))
+    #
+    #     m1 = sns.heatmap(accuracy_train, cmap='hot', ax=ax1, cbar=False, vmin=min_val, vmax=1)
+    #     m1.set_title('Training Accuracy')
+    #     m1.set_xlabel("$log_{10}$(c)")
+    #     m1.set_ylabel("$log_{10}$($\\gamma$)")
+    #     m1.set_xticklabels(ics)
+    #     m1.set_yticklabels(igs)
+    #
+    #     m2 = sns.heatmap(accuracy_dev, cmap='hot', ax=ax2, cbar_ax=cax, vmin=min_val, vmax=1)
+    #     m2.set_title('Validation Accuracy')
+    #     m2.set_xlabel("$log_{10}$(c)")
+    #     m2.set_ylabel("$log_{10}$($\\gamma$)")
+    #     m2.set_xticklabels(ics)
+    #     m2.set_yticklabels(igs)
+    #
+    #     plt.tight_layout()
+    #     plt.show()
 
-        m1 = sns.heatmap(accuracy_train, cmap='hot', ax=ax1, cbar=False, vmin=min_val, vmax=1)
-        m1.set_title('Training Accuracy')
-        m1.set_xlabel("$log_{10}$(c)")
-        m1.set_ylabel("$log_{10}$($\\gamma$)")
-        m1.set_xticklabels(ics)
-        m1.set_yticklabels(igs)
-
-        m2 = sns.heatmap(accuracy_dev, cmap='hot', ax=ax2, cbar_ax=cax, vmin=min_val, vmax=1)
-        m2.set_title('Validation Accuracy')
-        m2.set_xlabel("$log_{10}$(c)")
-        m2.set_ylabel("$log_{10}$($\\gamma$)")
-        m2.set_xticklabels(ics)
-        m2.set_yticklabels(igs)
-
-        plt.tight_layout()
-        plt.show()
-
-    if sv_plot:
-        plt.clf()
-        plt.plot(np.log10(gs), sv_c_constant)
-        plt.xlabel("$log_{10}$($\\gamma$)")
-        plt.ylabel("Number of Support Vectors")
-        plt.title("Number of Support Vectors for Varying $\gamma$-Values")
-        plt.show()
-
-        plt.clf()
-        plt.plot(np.log10(cs), sv_g_constant)
-        plt.xlabel("$log_{10}$(c)")
-        plt.ylabel("Number of Support Vectors")
-        plt.title("Number of Support Vectors for Varying C-Values")
-        plt.show()
+    # if sv_plot:
+    #     plt.clf()
+    #     plt.plot(np.log10(gs), sv_c_constant)
+    #     plt.xlabel("$log_{10}$($\\gamma$)")
+    #     plt.ylabel("Number of Support Vectors")
+    #     plt.title("Number of Support Vectors for Varying $\gamma$-Values")
+    #     plt.show()
+    #
+    #     plt.clf()
+    #     plt.plot(np.log10(cs), sv_g_constant)
+    #     plt.xlabel("$log_{10}$(c)")
+    #     plt.ylabel("Number of Support Vectors")
+    #     plt.title("Number of Support Vectors for Varying C-Values")
+    #     plt.show()
 
 if __name__ == "__main__":
     data_train = load_data("IA3-train.csv")
